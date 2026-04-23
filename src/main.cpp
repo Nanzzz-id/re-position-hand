18b5266d7635f4c5dbd978b3997e033523461f48
Nanzzz-id <luxxstill@gmail.com>
2026-04-24 02:35:09 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include <jni.h>
-#include <string>
 #include <android/log.h>
 
 #define LOG_TAG "LeviViewModel"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
-// Inisialisasi JNI sederhana untuk tes build
 extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
-    LOGI("Plugin LeviViewModel berhasil dimuat!");
+    LOGI("Plugin LeviViewModel Minimal Berhasil Dimuat!");
     return JNI_VERSION_1_6;
 }
